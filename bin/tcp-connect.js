394f390ea1b09a7f25fd8dcb0543d7acd934d47c
lachlan <lachlan@permafro.st>
2012-06-08 22:43:39 +1000

    Immediately close client socket on timeout

--- bin/tcp-connect.js
+++ bin/tcp-connect.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 var tcp = require('../lib/main');
-tcp.connect(process.argv.slice(2), function(err, results) {
-  process.exit(err == null ? 0 : 1);
+
+tcp.connect(process.argv.slice(2), function(result) {
+  process.exit(result ? 0 : 1);
 });
