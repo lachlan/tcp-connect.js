f862a2d7cc90b828dba0a13fb47ab39439af17a9
Lachlan Dowding <lachlan@permafro.st>
2012-06-14 11:38:50 +1000

    Just some cleanup

--- bin/tcp-connect.js
+++ bin/tcp-connect.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 var tcp = require('../lib/main');
 
-tcp.connect(process.argv.slice(2), function(result) {
-  process.exit(result ? 0 : 1);
+tcp.connect(process.argv.slice(2), function(connected) {
+  if (!connected) process.exit(1);
+});
+
+process.on('uncaughtException', function(err) {
+  console.log(err);
+  process.exit(1);
 });
